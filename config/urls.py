0786e8c23c2e4ded98a0eb9c2615ab39422155d6
LBH\user <leebh0806@naver.com>
2025-09-17 18:33:18 +0900

    login

--- config/urls.py
+++ config/urls.py
@@ -15,14 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.http import HttpResponse
-from django.urls import path
-from todo.views import todo_list, todo_info, main
-
+from django.urls import path, include
+from todo.views import todo_list, todo_info
+from users import views as user_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('todo/', todo_list, name='todo_list'),
     path('todo/<int:todo_id>/', todo_info, name='todo_info'),
-    path('', main, name='main'),
+    path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/login/', user_views.login, name='login'),
+    path('accounts/signup/', user_views.sign_up, name='signup')
 ]
