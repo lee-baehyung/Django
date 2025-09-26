02fa72e95907a4ae754906806141698b8dc5eb01
LBH\user <leebh0806@naver.com>
2025-09-26 16:14:18 +0900

    chapter_08

--- config/settings.py
+++ config/settings.py
@@ -11,18 +11,13 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import json
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+SECRET_KEY = '%tr+1w9vwi_x*nlx4d7ucvo9-_dz1n0w$k4$$$r)*es$c%*3w'
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-cwk(+62$r)$%6uln)drwyzhjjr8$24gre##qd&+fe+zlmq)i51'
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
@@ -167,3 +162,30 @@ SUMMERNOTE_CONFIG = {
     'disable_attachment': False,
     'attachment_absolute_uri': True,
 }
+
+AUTH_USER_MODEL = 'users.User'
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+with open(BASE_DIR / '.secret_config' / 'secret.json') as f:
+    config_secret_str = f.read()
+
+SECRET = json.loads(config_secret_str)
+
+SECRET_KEY = SECRET.get('DJANGO_SECRET_KEY')
+
+DEBUG = True
+
+
+AUTH_USER_MODEL = 'users.User'
+
+# email
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = SECRET['EMAIL']['USER']
+EMAIL_HOST_PASSWORD = SECRET['EMAIL']['PASSWORD']
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
